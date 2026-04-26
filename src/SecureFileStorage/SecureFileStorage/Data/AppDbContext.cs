4f2abb908de4da08613cecc71177924772ef44eb
yulianaqq <gnatyukulia2020@gmail.com>
2026-04-26 08:25:01 +0300

    Register DbContext and add EF Core design package
    Register AppDbContext with SQL Server in Program.cs using the "Default" connection string and add necessary EF Core usings. Add Microsoft.EntityFrameworkCore.Design to the project (PrivateAssets=all) to support design-time tools. Remove OpenAPI service/middleware and some pipeline calls (MapOpenApi, UseHttpsRedirection, UseAuthorization) leaving controller routing intact.

--- src/SecureFileStorage/SecureFileStorage/Data/AppDbContext.cs
+++ src/SecureFileStorage/SecureFileStorage/Data/AppDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SecureFileStorage.Models;
+using Microsoft.EntityFrameworkCore.Design;
+
 
 namespace SecureFileStorage.Data
 {
--- src/SecureFileStorage/SecureFileStorage/Program.cs
+++ src/SecureFileStorage/SecureFileStorage/Program.cs
@@ -1,22 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SecureFileStorage.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
