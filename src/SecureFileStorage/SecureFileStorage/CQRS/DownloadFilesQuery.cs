4ca8d2284f6d25fcb7ecd2dfb0451aec41613c6b
IHryhorenko <igrigorenco16@gmail.com>
2026-04-26 08:33:49 +0200

    Handlers ant other
    Створив DeleteFileCommand, DeleteFileHandler, DownloadFilesHandler, DownloadFilesQuery.

---
+++ src/SecureFileStorage/SecureFileStorage/CQRS/DownloadFilesQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SecureFileStorage.Models;
+
+namespace SecureFileStorage.CQRS.Queries
+{
+    public class DownloadFileQuery : IRequest<FileStreamResult>
+    {
+        public Guid Id { get; set; }
+    }
+}
